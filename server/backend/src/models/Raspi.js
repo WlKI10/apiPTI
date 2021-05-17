33fbe0a085a503b04543dc5308abe9b35e3873e8
eric.ruiz.gimenez <eric.ruiz.gimenez@estudiantat.upc.edu>
2021-05-17 12:24:46 +0200

    Backend con rutas de Raspi

--- server/backend/src/models/Raspi.js
+++ server/backend/src/models/Raspi.js
@@ -3,8 +3,8 @@ var bcrypt = require('bcrypt');
     SALT_WORK_FACTOR = 10;
 
 const raspiSchema = new Schema ({
-    username:{type:String,required:true,unique:false},
     serial_number:{type:String,required:true, unique:true},
+    username:{type:String,required:true,unique:false},
 });
 
 
